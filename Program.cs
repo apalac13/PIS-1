6d5843bf61add98ddc9e5346ba38b528ac9cd67d
student <Student@B420-01.fesb.hr>
2021-03-22 17:55:03 +0100

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace vjezbapis1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("danas je ponedjeljak");
         }
     }
 }
