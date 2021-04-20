f4cf7973f0831267ce291e740e6cd24808a3a95d
kkelam <81177703+kkelam@users.noreply.github.com>
2021-04-20 11:45:35 +0200

    Update Program.cs
    17. zad

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace vjezbapis1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("danas je ponedjeljak");
+            Console.WriteLine("Promjena");
         }
     }
 }
