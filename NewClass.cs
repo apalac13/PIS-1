f98b3091ba5a1b9cc7880fe6d8618510cf082c08
Korisnik <Korisnik@PALAC>
2021-04-21 18:08:10 +0200

    Dodana metoda

---
+++ NewClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace ConsoleApp1
+{
+    class NewClass
+    {
+        static void Metodica()
+        {
+
+        }
+    }
+}
