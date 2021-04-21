93e06824969b1553e01b253eebd628533c620ee2
kkelam <81177703+kkelam@users.noreply.github.com>
2021-04-21 18:10:59 +0200

    Update NewClass.cs
    Promjena od 19. zadatka

---
+++ NewClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class NewClass
+    {
+        static void Dodana_promjena()
+        {
+            
+        }
+    }
+}
